5aabefb5616cad105195476c7a6f16b6a686a956
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-19 21:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var fname=document.getElementById("fname");
+var lname=document.getElementById("lname");
+var phoneNumber=document.getElementById("phone-number");
+var emailID=document.getElementById("email");
+ 
+// var alertValue=` First Name:${fname.value} Last Name:${lname.value} Phone Number: ${phoneNumber.value}Email ID: ${ emailID.value} ` ;
+
+function alertTable(){
+    alert("First Name:"+fname.value+ " Last Name:"+lname.value+" Phone Number:"+phoneNumber.value+ " Email ID:"+emailID.value);
+}
